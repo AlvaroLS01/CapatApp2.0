50a7decfb4a6f2ac7b494c41e9ec6d3548182401
AlvaroLS01 <alvarito.lechuga@gmail.com>
2024-01-21 13:40:14 +0100

    Commit 2

--- app/src/main/java/com/alvarols01/capatapp2/MainActivity.kt
+++ app/src/main/java/com/alvarols01/capatapp2/MainActivity.kt
@@ -5,6 +5,13 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
+        //Esto lo hago para que se vea la pantalla de carga
+        Thread.sleep(2000)
+
+        setTheme(R.style.AppTheme)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
